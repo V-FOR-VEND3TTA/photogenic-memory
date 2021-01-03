8bbbd811909112e21d8e7b502481f90f5c1a2b7e
George Hlongwane <55334979+V-FOR-VEND3TTA@users.noreply.github.com>
2021-01-03 19:06:52 +0200

    Photogenic Memory V1.0
    First iteration of this website

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Animation of smooth scroll
+$("#view - work").on("click", function () {
+  const images = $("images").position().top;
+
+  $("html, body").animate(
+    {
+      scrollTop: images,
+    },
+    900
+  );
+});
